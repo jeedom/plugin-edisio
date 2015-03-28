01459bc35c3cf67ed45a4bb1b616ff901dca6965
Loic <loicgevrey@gmail.com>
2015-03-28 16:54:42 +0100

    use new log system

--- desktop/modal/show.debug.php
+++ desktop/modal/show.debug.php
@@ -22,11 +22,35 @@ echo '<div class="alert alert-warning">{{Attention le lancement en mode debug es
 sendVarToJs('debugMode_slaveId', init('slave_id'));
 ?>
 <div id='div_edisioShowDebug' style="display: none;"></div>
-<pre id='pre_edisiolog' style='overflow: auto; height: 85%;with:90%;'></pre>
+<a class="btn btn-warning pull-right" data-state="1" id="bt_edisioLogStopStart"><i class="fa fa-pause"></i> {{Pause}}</a>
+<input class="form-control pull-right" id="in_edisioLogSearch" style="width : 300px;" placeholder="{{Rechercher}}" />
+<br/><br/><br/>
+<pre id='pre_edisiolog' style='overflow: auto; height: 80%;with:90%;'></pre>
 
 
 <script>
- if(debugMode_slaveId != ''){
+    $('#bt_edisioLogStopStart').on('click',function(){
+        if($(this).attr('data-state') == 1){
+            $(this).attr('data-state',0);
+            $(this).removeClass('btn-warning').addClass('btn-success');
+            $(this).html('<i class="fa fa-play"></i> {{Reprise}}');
+
+        }else{
+            $(this).removeClass('btn-success').addClass('btn-warning');
+            $(this).html('<i class="fa fa-pause"></i> {{Pause}}');
+            $(this).attr('data-state',1);
+            jeedom.log.autoupdate({
+                log : 'edisiocmd',
+                slaveId : debugMode_slaveId,
+                display : $('#pre_edisiolog'),
+                search : $('#in_edisioLogSearch'),
+                control : $('#bt_edisioLogStopStart'),
+            });
+        }
+    });
+
+
+    if(debugMode_slaveId != ''){
  $.ajax({// fonction permettant de faire de l'ajax
             type: "POST", // methode de transmission des données au fichier php
             url: "plugins/edisio/core/ajax/edisio.ajax.php", // url du fichier php
@@ -44,52 +68,16 @@ sendVarToJs('debugMode_slaveId', init('slave_id'));
                 $('#div_edisioShowDebug').showAlert({message: data.result, level: 'danger'});
                 return;
             }
-             getJeedomSlaveLog(1);
+            jeedom.log.autoupdate({
+                log : 'edisiocmd',
+                slaveId : debugMode_slaveId,
+                display : $('#pre_edisiolog'),
+                search : $('#in_edisioLogSearch'),
+                control : $('#bt_edisioLogStopStart'),
+            });
         }
     });
 
-
-
- function getJeedomSlaveLog(_autoUpdate) {
-    $.ajax({
-        type: 'POST',
-        url: 'core/ajax/jeeNetwork.ajax.php',
-        data: {
-            action: 'getLog',
-            log: 'edisiocmd',
-            id: debugMode_slaveId
-        },
-        dataType: 'json',
-        global: false,
-        error: function (request, status, error) {
-            setTimeout(function () {
-                getJeedomSlaveLog(_autoUpdate, 'edisiocmd')
-            }, 1000);
-        },
-        success: function (data) {
-            if (data.state != 'ok') {
-                $('#div_edisioShowDebug').showAlert({message: data.result, level: 'danger'});
-                return;
-            }
-            var log = '';
-            var regex = /<br\s*[\/]?>/gi;
-            for (var i in data.result.reverse()) {
-                log += data.result[i][2].replace(regex, "\n");
-            }
-            $('#pre_edisiolog').text(log);
-            $('#pre_edisiolog').scrollTop($('#pre_edisiolog').height() + 200000);
-            if (!$('#pre_edisiolog').is(':visible')) {
-                _autoUpdate = 0;
-            }
-
-            if (init(_autoUpdate, 0) == 1) {
-                setTimeout(function () {
-                    getJeedomSlaveLog(_autoUpdate)
-                }, 1000);
-            }
-        }
-    });
-}
 }else{
     $.ajax({
         type: 'POST',
@@ -107,48 +95,13 @@ sendVarToJs('debugMode_slaveId', init('slave_id'));
                 $('#div_edisioShowDebug').showAlert({message: data.result, level: 'danger'});
                 return;
             }
-            getDebugLog(1);
+            jeedom.log.autoupdate({
+                log : 'edisiocmd',
+                display : $('#pre_edisiolog'),
+                search : $('#in_edisioLogSearch'),
+                control : $('#bt_edisioLogStopStart'),
+            });
         }
     });
-
-    function getDebugLog(_autoUpdate) {
-        $.ajax({
-            type: 'POST',
-            url: 'core/ajax/log.ajax.php',
-            data: {
-                action: 'get',
-                logfile: 'edisiocmd',
-            },
-            dataType: 'json',
-            global: false,
-            error: function (request, status, error) {
-                setTimeout(function () {
-                    getJeedomLog(_autoUpdate, 'edisiocmd')
-                }, 1000);
-            },
-            success: function (data) {
-                if (data.state != 'ok') {
-                    $('#div_edisioShowDebug').showAlert({message: data.result, level: 'danger'});
-                    return;
-                }
-                var log = '';
-                var regex = /<br\s*[\/]?>/gi;
-                for (var i in data.result.reverse()) {
-                    log += data.result[i][2].replace(regex, "\n");
-                }
-                $('#pre_edisiolog').text(log);
-                $('#pre_edisiolog').scrollTop($('#pre_edisiolog').height() + 200000);
-                if (!$('#pre_edisiolog').is(':visible')) {
-                    _autoUpdate = 0;
-                }
-
-                if (init(_autoUpdate, 0) == 1) {
-                    setTimeout(function () {
-                        getDebugLog(_autoUpdate)
-                    }, 1000);
-                }
-            }
-        });
-}
 }
 </script>
