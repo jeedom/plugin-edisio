24fae72abf19324192e0fe20f380cc01e302826f
Loic <loicgevrey@gmail.com>
2015-08-23 13:12:20 +0200

    Security fix

--- core/php/jeeEdisio.php
+++ core/php/jeeEdisio.php
@@ -17,7 +17,7 @@
  */
 require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 
-if (config::byKey('api') != init('apikey') && init('apikey') != '') {
+if (config::byKey('api') != init('apikey')) {
 	connection::failed();
 	echo 'Clef API non valide, vous n\'êtes pas autorisé à effectuer cette action';
 	die();
