e0cb814946366072093c0f9d63463a875fe3b167
Loïc <loic@jeedom.com>
2015-10-25 10:55:18 +0100

    Try improvement

--- core/php/jeeEdisio.php
+++ core/php/jeeEdisio.php
@@ -44,7 +44,7 @@ if (!is_object($edisio)) {
 if ($_GET['mid'] == 1 || $_GET['mid'] == 2 || $_GET['mid'] == 3 || $_GET['mid'] == 4 || $_GET['mid'] == 5 || $_GET['mid'] == 7 || $_GET['mid'] == 9) {
 	$logicalId = 'bt' . $_GET['bt'];
 	$value = $_GET['value'];
-	$cmdArray = array($logicalId,$logicalId. 'long');
+	$cmdArray = array($logicalId, $logicalId . 'long');
 	foreach ($cmdArray as $logicalIdToCheck) {
 		$cmd = $edisio->getCmd('info', $logicalIdToCheck);
 		if (!is_object($cmd) && config::byKey('autoDiscoverEqLogic', 'edisio') != 0) {
@@ -64,8 +64,8 @@ if ($_GET['mid'] == 1 || $_GET['mid'] == 2 || $_GET['mid'] == 3 || $_GET['mid'] 
 			$cmd->save();
 		}
 	}
-	if (in_array($value,array("up", "down"))) {
-		$cmd = $edisio->getCmd('info', $logicalId. 'long');
+	if (in_array($value, array('up', 'down'))) {
+		$cmd = $edisio->getCmd('info', $logicalId . 'long');
 	} else {
 		$cmd = $edisio->getCmd('info', $logicalId);
 	}
@@ -73,27 +73,24 @@ if ($_GET['mid'] == 1 || $_GET['mid'] == 2 || $_GET['mid'] == 3 || $_GET['mid'] 
 		if ($value == 'toggle' && $cmd->getSubType() == 'binary') {
 			$value = $cmd->execCmd();
 			$value = ($value != 0) ? 0 : 1;
-		}
-		else if ($value == 'toggle' && $cmd->getSubType() == 'numeric') {
+		} else if ($value == 'toggle' && $cmd->getSubType() == 'numeric') {
 			$value = $cmd->execCmd();
 			$value = ($value != 0) ? $cmd->getConfiguration('minValue', 0) : $cmd->getConfiguration('maxValue', 100);
-		}
-		else if ($value == 'up' && $cmd->getSubType() == 'binary') {
+		} else if ($value == 'up' && $cmd->getSubType() == 'binary') {
 			$value = 1;
-		}
-		else if ($value == 'down' && $cmd->getSubType() == 'binary') {
+		} else if ($value == 'down' && $cmd->getSubType() == 'binary') {
 			$value = 0;
-		}
-		else if ($value == 'up' && $cmd->getSubType() == 'numeric') {
+		} else if ($value == 'up' && $cmd->getSubType() == 'numeric') {
 			$value = $cmd->execCmd();
 			$range = $cmd->getConfiguration('maxValue', 100) - $cmd->getConfiguration('minValue', 0);
 			$value += $range / 10;
 			if ($value > $cmd->getConfiguration('maxValue', 100)) {
 				$value = $cmd->getConfiguration('minValue', 0);
 			}
-		};
+		}
 		$cmd->event($value);
 	}
+	die();
 }
 
 foreach ($edisio->getCmd('info') as $cmd) {
