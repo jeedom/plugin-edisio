13bd5874859adb466f6b488070842f44ac1e5a3e
Loïc <loic@jeedom.com>
2016-05-11 11:40:15 +0200

    Remove useless import + decode string

--- resources/edisiod/edisiod.py
+++ resources/edisiod/edisiod.py
@@ -28,7 +28,6 @@ import threading
 from threading import Thread, Event, Timer
 import re
 import signal
-import xml.dom.minidom as minidom
 from optparse import OptionParser
 import socket
 import select
@@ -151,181 +150,120 @@ def decodePacket(message):
 		if clean_message == _prevMessage and unixtime_utc_check < (_prevDatetime+datetime.timedelta(milliseconds=200)) :
 			logging.debug("Message already decode, ignore")
 			return
+			
 	_prevMessage = clean_message
 	_prevDatetime = unixtime_utc_check
-	decode_string = "\nPID\t\t\t= " + str(PID);
-	decode_string += "\nBID\t\t\t= " + str(BID);
-	decode_string += "\nMID\t\t\t= " + str(MID);
-	decode_string += "\nBL\t\t\t= " + str(BL);
-	decode_string += "\nRMAX\t\t\t= " + str(RMAX);
-	decode_string += "\nRC\t\t\t= " + str(RC);
-	decode_string += "\nCMD\t\t\t= " + str(CMD);
-	decode_string += "\nDATA\t\t\t= " + str(DATA);
-	
 	BL = int((int(BL, 16) / 3.3) * 10)
 	action = {'id' : str(PID), 'battery' : str(BL), 'mid' : str(MID)}
 	key = str(PID)+str(MID)+str(CMD)+str(BID)
 	value = ''
 
 	if CMD == '01':
-		decode_string += "\nCommand value : \t= ON"
 		value = 1
 	if CMD == '02':
-		decode_string += "\nCommand value : \t= OFF"
 		value = 0
 	if CMD == '03':
-		decode_string += "\nCommand value : \t= Toogle"
 		value = 'toggle'
 	if CMD == '04':
-		decode_string += "\nCommand value : \t= Dim"
 		value = 'toggle'
 	if CMD == '05':
-		decode_string += "\nCommand value : \t= Dim up"
 		value = 'toggle'
 	if CMD == '06':
-		decode_string += "\nCommand value : \t= Dim down"
 		value = 'toggle'
 	if CMD == '07':
-		decode_string += "\nCommand value : \t= Dim A"
 		value = 'up'
 	if CMD == '08':
-		decode_string += "\nCommand value : \t= Dim stop"
 		value = 'toggle'
 	if CMD == '09':
-		decode_string += "\nCommand value : \t= Shutter open"
 		value = 1
 	if CMD == '0A':
-		decode_string += "\nCommand value : \t= Shutter close"
 		value = 0
 	if CMD == '0B':
-		decode_string += "\nCommand value : \t= Shutter stop"
 		value = 0
 	if CMD == '0C':
-		decode_string += "\nCommand value : \t= RGB"
 		value = 0
 	if CMD == '0D':
-		decode_string += "\nCommand value : \t= RGB c"
 		value = 0
 	if CMD == '0E':
-		decode_string += "\nCommand value : \t= RGB plus"
 		value = 0
 	if CMD == '0F':
-		decode_string += "\nCommand value : \t= Open slow"
 		value = 0
 	if CMD == '10':
-		decode_string += "\nCommand value : \t= Set short"
 		value = 0
 	if CMD == '11':
-		decode_string += "\nCommand value : \t= Set 5s"
 		value = 0
 	if CMD == '12':
-		decode_string += "\nCommand value : \t= Set 10s"
 		value = 0
 	if CMD == '13':
-		decode_string += "\nCommand value : \t= Close slow"
 		value = 0
 	if CMD == '14':
-		decode_string += "\nCommand value : \t= Dim stop2"
 		value = 0
 	if CMD == '15':
-		decode_string += "\nCommand value : \t= Set long up"
 		value = 0
 	if CMD == '16':
-		decode_string += "\nCommand value : \t= Study"
 		value = 0
 	if CMD == '17':
-		decode_string += "\nCommand value : \t= Del button"
 		value = 0
 	if CMD == '18':
-		decode_string += "\nCommand value : \t= Del all"
 		value = 0
 	if CMD == '19':
-		decode_string += "\nCommand value : \t= Door close"
 		value = 0
 	if CMD == '1A':
-		decode_string += "\nCommand value : \t= Door open"
 		value = 1
 	if CMD == '1F':
-		decode_string += "\nCommand value : \t= Broadcast query"
 		value = 0
 	if CMD == '20':
-		decode_string += "\nCommand value : \t= Querry status"
 		value = 0
 	if CMD == '21':
-		decode_string += "\nCommand value : \t= Report status"
 		value = 0
 	if CMD == 'F1':
-		decode_string += "\nCommand value : \t= Dim to 10%"
 		value = 10
 	if CMD == 'F2':
-		decode_string += "\nCommand value : \t= Dim to 20%"
 		value = 20
 	if CMD == 'F3':
-		decode_string += "\nCommand value : \t= Dim to 30%"
 		value = 30
 	if CMD == 'F4':
-		decode_string += "\nCommand value : \t= Dim to 40%"
 		value = 40
 	if CMD == 'F5':
-		decode_string += "\nCommand value : \t= Dim to 50%"
 		value = 50
 	if CMD == 'F6':
-		decode_string += "\nCommand value : \t= Dim to 60%"
 		value = 60
 	if CMD == 'F7':
-		decode_string += "\nCommand value : \t= Dim to 70%"
 		value = 70
 	if CMD == 'F8':
-		decode_string += "\nCommand value : \t= Dim to 80%"
 		value = 80
 	if CMD == 'F9':
-		decode_string += "\nCommand value : \t= Dim to 90%"
 		value = 90
 	if CMD == 'FA':
-		decode_string += "\nCommand value : \t= Dim to 100%"
 		value = 100
 
 	if MID == '01':
-		decode_string += "\nDecode model : \t\t= Emitter Channel 1"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '02':
-		decode_string += "\nDecode model : \t\t= Emitter Channel 2"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '03':
-		decode_string += "\nDecode model : \t\t= Emitter Channel 3"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '04':
-		decode_string += "\nDecode model : \t\t= Emitter Channel 4"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '05':
-		decode_string += "\nDecode model : \t\t= Emitter Channel 5"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '06':
-		decode_string += "\nDecode model : \t\t= Energy Meter"
-
+		return
 	if MID == '07':
-		decode_string += "\nDecode model : \t\t= Motion Sensor (On/Off/Pulse)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '08':
-		decode_string += "\nDecode model : \t\t= Temperature Sensor"
 		try:
 			temperature = float(int(DATA[3:4]+DATA[0:2],16)) / 100
 		except Exception, e:
@@ -333,72 +271,50 @@ def decodePacket(message):
 			return
 		action['temperature'] = str(temperature)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '09':
 		decode_string += "\nDecode model : \t\t= Door Sensor (On/Off/Pulse)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '10':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (On/Off/Pulse)"
-
+		return
 	if MID == '11':
-		decode_string += "\nDecode model : \t\t= Receiver 2 Outputs (On/Off or Pulse)"
-
+		return
 	if MID == '12':
-		decode_string += "\nDecode model : \t\t= Receiver 4 Outputs (On/Off or Dimer)"
-
+		return
 	if MID == '13':
-		decode_string += "\nDecode model : \t\t= Receiver 4 Outputs (On/Off/Pulse or 2x Open/Close)"
-
+		return
 	if MID == '14':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (On/Off or Dimer)"
-
+		return
 	if MID == '15':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (On/Off or Dimer)"
-
+		return
 	if MID == '16':
-		decode_string += "\nDecode model : \t\t= Gateway (esaylink)"
-
+		return
 	if MID == '17':
-		decode_string += "\nDecode model : \t\t= Emitter 2 Channels (Button)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '18':
-		decode_string += "\nDecode model : \t\t= Emitter 2 Channels (Button)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '0B':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (On/Off)"
-
+		return
 	if MID == '0E':
-		decode_string += "\nDecode model : \t\t= Thermostat"
-
+		return
 	if MID == '0F':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (Heater On/Off)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '0C':
-		decode_string += "\nDecode model : \t\t= Receiver 1 Output (Pilot Wire Functions)"
 		action['bt'] = str(BID)
 		action['value'] = str(value)
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '0D':
-		decode_string += "\nDecode model : \t\t= Receiver 2 Outputs (1x Open/Close)"
-
+		return
 	if MID == '1B':
-		decode_string += "\nDecode model : \t\t= IR transmitter (Trigger)"
-
+		return
 	if MID == '1D':
-		decode_string += "\nDecode model : \t\t= EMC-CAR (Trigger)"
 		if CMD == '0B':
 			action['state'] = '1'
 		if CMD == '0A':
@@ -406,87 +322,61 @@ def decodePacket(message):
 		if CMD == '09':
 			action['state'] = '3'
 		jeedom_com.add_changes('devices::'+str(key),action);
-
 	if MID == '1E':
-		decode_string += "\nDecode model : \t\t= Enocean Sensor (Door)"
-
+		return
 	if MID == '1F':
-		decode_string += "\nDecode model : \t\t= Enocean Sensor (Temperature)"
-
+		return
 	if MID == '20':
-		decode_string += "\nDecode model : \t\t= Enocean Switch (Switch)"
-
+		return
 	if MID == '21':
-		decode_string += "\nDecode model : \t\t= Virtual Device (IR Remote Control)"
-
+		return
 	if MID == '22':
-		decode_string += "\nDecode model : \t\t= Enocean (Motion Sensor)"
-
+		return
 	if MID == '23':
-		decode_string += "\nDecode model : \t\t= Enocean socket"
-
-	logging.debug("Decode packet : "+decode_string)
+		return
 	return
 
 # ----------------------------------------------------------------------------
 
 def test_edisio( message ):
-	# Remove all invalid characters
 	message = jeedom_utils.stripped(message)
-	
-	# Remove any whitespaces
 	try:
 		message = message.replace(' ', '')
 	except Exception:
 		logging.debug("Error: Removing white spaces")
 		return False
-	
-	# Test the string if it is hex format
 	try:
 		int(message,16)
 	except Exception:
 		logging.debug("Error: Packet not hex format")
 		return False
-	
-	# Check that length is even
 	if len(message) % 2:
 		logging.debug("Error: Packet length not even")
 		return False
-
 	if len(message.decode('hex')) < 16:
 		logging.debug("Error: Packet length is not valid (<16)")
 		return False
-	
-	# Check that first byte is not 6C
 	if jeedom_utils.ByteToHex(message.decode('hex')[0]) <> "6C":
 		logging.debug("Error: Packet first byte is not 6C : "+str(jeedom_utils.ByteToHex(message.decode('hex')[0])))
 		return False
-
 	if jeedom_utils.ByteToHex(message.decode('hex')[1]) <> "76":
 		logging.debug("Error: Packet second byte is not 76 : "+str(jeedom_utils.ByteToHex(message.decode('hex')[0])))
 		return False
-
 	if jeedom_utils.ByteToHex(message.decode('hex')[2]) <> "63":
 		logging.debug("Error: Packet third byte is not 63 : "+str(jeedom_utils.ByteToHex(message.decode('hex')[0])))
 		return False
-
 	if jeedom_utils.ByteToHex(message.decode('hex')[-1]) <> "0A":
 		logging.debug("Error: Packet last byte is not 0A : "+str(jeedom_utils.ByteToHex(message.decode('hex')[-1])))
 		return False
-
 	if jeedom_utils.ByteToHex(message.decode('hex')[-2]) <> "0D":
 		logging.debug("Error: Packet -2 byte is not 0D : "+str(jeedom_utils.ByteToHex(message.decode('hex')[-2])))
 		return False
-
 	if jeedom_utils.ByteToHex(message.decode('hex')[-3]) <> "64":
 		logging.debug("Error: Packet -3 byte is not 64 : "+str(jeedom_utils.ByteToHex(message.decode('hex')[-3])))
 		return False
-	
-	# Length more than one byte
 	if not len(message.decode('hex')) > 1:
 		logging.debug("Error: Packet is not longer than one byte")
 		return False
-
 	return True
 			
 # ----------------------------------------------------------------------------
