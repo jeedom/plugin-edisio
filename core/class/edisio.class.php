38a2a2555938481f24c26e01f582a9d88110961d
Loïc <loic@jeedom.com>
2017-01-25 12:37:22 +0100

    Use new dependancy system

--- core/class/edisio.class.php
+++ core/class/edisio.class.php
@@ -117,10 +117,8 @@ class edisio extends eqLogic {
 		return $return;
 	}
 	public static function dependancy_install() {
-		log::remove('edisio_update');
-		$cmd = 'sudo /bin/bash ' . dirname(__FILE__) . '/../../resources/install.sh';
-		$cmd .= ' >> ' . log::getPathToLog('edisio_dependancy') . ' 2>&1 &';
-		exec($cmd);
+		log::remove(__CLASS__ . '_update');
+		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh', 'log' => log::getPathToLog(__CLASS__ . '_update'));
 	}
 
 	public static function deamon_info() {
