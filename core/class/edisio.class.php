9cd8ef9523d5d9d565668fbedc499871334e92bc
Loïc <loic@jeedom.com>
2017-03-20 16:09:30 +0100

    Unify dependy installation

--- core/class/edisio.class.php
+++ core/class/edisio.class.php
@@ -108,7 +108,7 @@ class edisio extends eqLogic {
 
 	public static function dependancy_info() {
 		$return = array();
-		$return['progress_file'] = '/tmp/dependancy_edisio_in_progress';
+		$return['progress_file'] = jeedom::getTmpFolder('edisio') . '/dependance';
 		if (exec(system::getCmdSudo() . system::get('cmd_check') . '-E "python\-serial|python\-request|python\-pyudev" | wc -l') >= 3) {
 			$return['state'] = 'ok';
 		} else {
@@ -118,7 +118,7 @@ class edisio extends eqLogic {
 	}
 	public static function dependancy_install() {
 		log::remove(__CLASS__ . '_update');
-		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh', 'log' => log::getPathToLog(__CLASS__ . '_update'));
+		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('edisio') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
 	}
 
 	public static function deamon_info() {
